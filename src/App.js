27c3bbb73efcec4a273098980d5f85efb9e91fe3
Luke hwang <luke.hwang@buzzvil.com>
2020-11-18 22:09:15 +0900

    generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import Button from "@material-ui/core/Button";
+import Card from '@material-ui/core/Card';
 
-function App() {
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const [result, setResult] = useState({});
+  const onSubmit = (data) => {
+    const value = {
+      version: data.version,
+      notes: [
+        { role: "staff", lang: "ko", body: data.staffKo },
+        { role: "staff", lang: "en", body: data.staffEn },
+        { role: "publisher", lang: "ko", body: data.publisherKo },
+        { role: "publisher", lang: "en", body: data.publisherEn },
+        { role: "advertiser", lang: "ko", body: data.advertiserKo },
+        { role: "advertiser", lang: "en", body: data.advertiserEn },
+      ],
+    };
+    setResult(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField label="Version" name="version" inputRef={register} />
+        <TextField
+          multiline={true}
+          label="스태프 한글"
+          name="staffKo"
+          inputRef={register}
+        />
+        <TextField
+          multiline={true}
+          label="스태프 영어"
+          name="staffEn"
+          inputRef={register}
+        />
+        <TextField
+          multiline={true}
+          label="퍼블리셔 한글"
+          name="publisherKo"
+          inputRef={register}
+        />
+        <TextField
+          multiline={true}
+          label="퍼블리셔 영어"
+          name="publisherEn"
+          inputRef={register}
+        />
+        <TextField
+          multiline={true}
+          label="광고주 한글"
+          name="advertiserKo"
+          inputRef={register}
+        />
+        <TextField
+          multiline={true}
+          label="광고주 영어"
+          name="advertiserEn"
+          inputRef={register}
+        />
+        <Button type="submit" variant="contained" color="primary">
+          생성
+        </Button>
+      </form>
+      <div className="result">{Object.keys(result).length ? JSON.stringify(result) : ""}</div>
     </div>
   );
 }
-
-export default App;
